d294a52b1c380d966ca35895e078b8a6c504b9b2
hachi922 <141776530+hachi922@users.noreply.github.com>
2026-05-19 20:27:36 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'pantryno-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css',
+  'https://fonts.googleapis.com/css2?family=Pacifico&display=swap'
+];
+
+// インストール時にキャッシュ
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return Promise.allSettled(ASSETS.map(url => cache.add(url).catch(() => {})));
+    })
+  );
+  self.skipWaiting();
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// ネットワーク優先、失敗時にキャッシュにフォールバック
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
